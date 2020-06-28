91114b1fc1d58924dce0a1aa6f0bee1bdb5ab502
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-06-28 18:22:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board = 0)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   "+ "| #{board[4]} |" + "   "
+  puts " #{board(3)} "+ "| #{board[4]} |" + " #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 board = [" "," "," "," ","X","","","",""]
